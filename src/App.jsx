21e24acd4b44af3599c24a64a3770b8dc90a1433
Omar Hossam <omarhossam5467@gmail.com>
2025-07-09 12:06:03 +0300

    Finished the first version of this learning project app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function ComponentOne() {
   return (
     <div className="p-8 bg-gray-600 text-white m-3 rounded-xl">
@@ -62,10 +64,57 @@ function LoginPage() {
 
 let isLoggedIn = false;
 
-export default function App() {
+function ListComponent() {
+  const students = [
+    { name: "Omar", isAtCampus: true, id: 1 },
+    { name: "Hany", isAtCampus: true, id: 2 },
+    { name: "Ahmed", isAtCampus: false, id: 3 },
+    { name: "Osama", isAtCampus: true, id: 4 },
+    { name: "Farouk", isAtCampus: false, id: 5 },
+    { name: "Ziad", isAtCampus: true, id: 6 }
+  ]
+
+  const listItems = students.map(student => 
+    <li key={student.id} style={{
+      color: student.isAtCampus ? "green" : "red"
+    }}>
+      {student.name}
+    </li>
+  )
   return (
-    <div>
-      {isLoggedIn ? (<AdminPage />) : (<LoginPage />)}
-    </div>
+    <ul>{listItems}</ul>
+  )
+}
+
+function CounterButton({onClick, content}) {
+  return (
+    <button className="bg-gray-50 border-2 border-gray-200 cursor-pointer hover:bg-gray-100 active:bg-gray-200 hover:shadow-xl font-mono p-3.5  rounded-xl" onClick={onClick} style={{
+      color: content === "Increase" ? "green" : content === "Decrease" ? "darkred" : ""
+    }}>{content}</button>
+  )
+}
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  function incrementCount() {
+    setCount(count + 1)
+  }
+  function restoreCount() {
+    setCount(0)
+  }
+  function decrementCount() {
+    setCount(count - 1)
+  }
+
+  return (
+    <>
+      <div className="p-2 flex gap-3">
+        <CounterButton onClick={incrementCount} content="Increase" />
+        <CounterButton onClick={decrementCount} content="Decrease" />
+        <CounterButton onClick={restoreCount} content="Restore" />
+      </div>
+      <p className="font-sans p-2 text-2xl">Count = {count}</p>
+    </>
   )
 }

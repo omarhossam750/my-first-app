c35a69bebb7fafcdd6c5665788d12c3b41e8a49f
Omar Hossam <omarhossam5467@gmail.com>
2025-07-10 10:22:04 +0300

    Finished the initial version of the todo list app.

--- components/todos.jsx
+++ components/todos.jsx
@@ -1,25 +1,33 @@
-import { useRef } from "react"
+import { useRef, useState, useEffect } from "react";
 
 export default function TodoApp() {
   const inputRef = useRef(null);
-    
-  const todos = [{
-    id: 1, title: "Study Coding"
-  }];
+  
+  let [todos, setTodos] = useState([]);
+
   function addTodo(todoTitle) {
-    if (todoTitle === "") {
+    const trimmedTitle = todoTitle.trim()
+    if (trimmedTitle === "") {
       window.alert("Please enter a valid todo title!")
       return;
     }
-    todos.push({id: todos.length + 1, title: todoTitle})
+    
+    setTodos(prevTodos => [
+      ...prevTodos, 
+      {id: Date.now(), title: trimmedTitle}
+    ])
+    
     inputRef.current.value = "";
   }
 
   return (
-    <div className="max-w-md bg-gray-50 border-2 border-slate-200 rounded-sm mx-auto mt-2">
+    <div className="max-w-2xl bg-gray-50 border-2 border-slate-200 rounded-lg mx-auto mt-2">
       <div className="flex p-4">
-        <input ref={inputRef} className="border-slate-200 p-1.5 border-2 border-r-0 rounded-l-lg outline-0 focus:border-blue-500" type="text" placeholder="Enter todo title." />
-        <button className="bg-slate-800 rounded-r-lg text-white font-sans w-full cursor-pointer" onClick={() => addTodo(inputRef.current.value)}>Add Todo</button>
+        {/* INPUT */}
+        <input ref={inputRef} className="border-slate-200 p-1.5 border-2 w-2/3 border-r-0 rounded-l-lg outline-0 focus:border-blue-500" type="text" placeholder="Enter todo title." />
+        
+        {/* BUTTON */}
+        <button className="bg-slate-800 rounded-r-lg text-white font-sans w-1/3 cursor-pointer" onClick={() => addTodo(inputRef.current.value)}>Add Todo</button>
       </div>
       <ol className="pl-5 mb-4 list-decimal list-inside">
         {todos.map(todo => 
